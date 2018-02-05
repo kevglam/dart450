5a38c9b287534c6aef97b0a51da34b603529e74c
Kevin G Lam <kevlam10@outlook.com>
2018-02-05 12:55:29 -0500

    Exercise 04
    uploaded exercise 4

--- exercises/exercise04/js/script.js
+++ exercises/exercise04/js/script.js
@@ -8,8 +8,51 @@ Description of what the script does...
 
 **********************************************/
 
-$(document).ready() {
+$(document).ready(function () {
+
+  $('#Option1').on('click',function() {
+    $('#exampleDiv').prepend('<img id="dawey"  src="http://i65.tinypic.com/2090qvo.png" />')
+
+  });
+
+  $('#Option2').on('click',function() {
+
+    $('#exampleDiv').prepend('<img id="dawey"  src="http://i65.tinypic.com/2cxb5fo/9" />')
+  });
+
+  $('#Option3').on('click',function() {
+
+    $('#exampleDiv').prepend('<img id="dawey"  src="http://i65.tinypic.com/2ms3fyq.gif" />')
+  });
+
+  $('#Option4').on('click',function() {
+
+    $('#exampleDiv').prepend('<img id="dawey"  src="http://i65.tinypic.com/2rhwxw4.png" />')
+  });
+
+  $('#Option5').on('click',function() {
+
+    $('#exampleDiv').prepend('<img id="dawey"  src="http://i65.tinypic.com/2rgjcxl.gif" />')
+  });
+
+  $('#Option6').on('click',function() {
+
+    $('#exampleDiv').prepend('<img id="dawey"  src="http://i66.tinypic.com/r1kllw.jpg" />')
+  });
+
+  $('#Option7').on('click',function() {
+
+    $('#exampleDiv').prepend('<img id="dawey"  src="http://i67.tinypic.com/xvg37.gif" />')
+  });
+
+  //$( "body" ).mousemove(function(event) {
+
+  //$('body').prepend('<img id="dawey"  src="http://i65.tinypic.com/2090qvo.png" />')
+  //$('#dawey').css("transform","translate(100px, 100px)")
+  //$('#dawey').css("transform","translate(-100px, -100px)")
+  //console.log (event.pageX);
+  //});
 
   // Insert jQuery code here to run when the page is loaded
-  
-}
+
+});
